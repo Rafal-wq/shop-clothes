466ea011d94fda4054b3cca51af8b04e88ff3d2f
Rafal-wq <rafal_wilczewski@wp.pl>
2023-04-23 21:14:38 +0200

    Divide BLL and operations procedure with db

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,12 @@
 const mysql2 = require('mysql2/promise');
+require('dotenv').config();
 
 const pool = mysql2.createPool({
-    host: 'localhost',
-    port: '8889',
-    user: 'root',
-    password: 'root',
-    database: 'solvd_shop',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     namedPlaceholders: true,
     decimalNumbers: true,
 });
